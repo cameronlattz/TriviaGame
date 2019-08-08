0febd2f8605ee32b811442fa3b3a826e8d8511b0
Cameron Lattz <camerolattz@gmail.com>
2019-08-08 11:13:29 -0500

    showing answers between questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,7 @@ const app = (function() {
     let questions = [];
     let currentQuestion = 0;
     let timerInterval;
+    let paused = false;
     
     const convertToMinutesAndSeconds = function(timer) {
         minutes = parseInt(timer / 60, 10)
@@ -19,6 +20,7 @@ const app = (function() {
         return [minutes, seconds];
     }
     const clickAnswer = function(answerElement) {
+        paused = true;
         const lis = document.querySelectorAll("#answers li");
         for (let i = 0; i < lis.length; i++) {
             if (lis[i] === answerElement) {
@@ -26,49 +28,7 @@ const app = (function() {
             }
         }
         clearInterval(timerInterval);
-        startTimer(questionDuration);
-        if (currentQuestion === questions.length - 1) {
-            document.getElementById("textContainer").style.display = "block";
-            document.getElementById("questionContainer").style.display = "none";
-            let right = 0;
-            for (let i = 0; i < questions.length; i++) {
-                if (questions[i].correct_answer === questions[i].guess) {
-                    right++;
-                }
-            }
-            document.getElementById("textContainer").innerHTML = "<h1>You got " + right + " out of " + questions.length + " correct!</h1>";
-            document.getElementById("textContainer").innerHTML += "<h1>" + Math.round(100*right/questions.length) + "%</h1>";
-            for (let i = 0; i < questions.length; i++) {
-                const containerElement = document.createElement("div");
-                const questionElement = document.createElement("div");
-                questionElement.classList.add("question");
-                questionElement.innerHTML = questions[i].question;
-                containerElement.append(questionElement);
-                const answersElement = document.createElement("ul");
-                for (let j = 0; j < questions[i].answers.length; j++) {
-                    const answerElement = document.createElement("li");
-                    answerElement.innerHTML = questions[i].answers[j];
-                    if (questions[i].answers[j] === questions[i].correct_answer) {
-                        answerElement.classList.add("correct");
-                        answerElement.innerHTML += " (correct answer)";
-                    }
-                    if (questions[i].answers[j] === questions[i].guess) {
-                        answerElement.classList.add("guessed");
-                        answerElement.innerHTML += " (your guess)";
-                    }
-                    answersElement.append(answerElement);
-                }
-                containerElement.append(answersElement);
-                document.getElementById("textContainer").append(containerElement);
-            }
-            document.getElementById("textContainer").innerHTML += "<input type=\"button\" id=\"continueButton\" value=\"Try Again?\">";
-            document.getElementById("continueButton").addEventListener("click", function() {
-                location.reload();
-            });
-        } else {
-            currentQuestion++;
-            updateQuestion();
-        }
+        displayAnswer();
     };
     // sometimes we get html codes from the api. removing them here
     const decodeHtml = function(html) {
@@ -77,6 +37,61 @@ const app = (function() {
         let string = fakeElement.textContent;
         fakeElement.remove();
         return string;
+    }
+    const displayAnswer = function() {
+        const lis = document.getElementById("answers").querySelectorAll("li");
+        for (let i = 0; i < lis.length; i++) {
+            const li = lis[i];
+            const question = questions[currentQuestion];
+            if (li.textContent === question.correct_answer) {
+                li.classList.add("correct");
+                li.textContent += " (correct answer)";
+            } else if (li.textContent === question.guess) {
+                li.classList.add("guessed");
+            }
+        }
+        currentQuestion++;
+        setTimeout(updateQuestion, 3000);
+    }
+    const displayResults = function() {
+        document.getElementById("textContainer").style.display = "block";
+        document.getElementById("questionContainer").style.display = "none";
+        let right = 0;
+        for (let i = 0; i < questions.length; i++) {
+            if (questions[i].correct_answer === questions[i].guess) {
+                right++;
+            }
+        }
+        document.getElementById("textContainer").innerHTML = "<h1>You got " + right + " out of " + questions.length + " correct!</h1>";
+        document.getElementById("textContainer").innerHTML += "<h1>" + Math.round(100*right/questions.length) + "%</h1>";
+        for (let i = 0; i < questions.length; i++) {
+            const containerElement = document.createElement("div");
+            const questionElement = document.createElement("div");
+            questionElement.classList.add("question");
+            questionElement.innerHTML = questions[i].question;
+            containerElement.append(questionElement);
+            const answersElement = document.createElement("ul");
+            for (let j = 0; j < questions[i].answers.length; j++) {
+                const answerElement = document.createElement("li");
+                answerElement.innerHTML = questions[i].answers[j];
+                if (questions[i].answers[j] === questions[i].correct_answer) {
+                    answerElement.classList.add("correct");
+                    answerElement.innerHTML += " (correct answer)";
+                }
+                if (questions[i].answers[j] === questions[i].guess) {
+                    answerElement.classList.add("guessed");
+                    answerElement.innerHTML += " (your guess)";
+                }
+                answersElement.append(answerElement);
+            }
+            containerElement.append(answersElement);
+            document.getElementById("textContainer").append(containerElement);
+        }
+        document.getElementById("textContainer").innerHTML += "<input type=\"button\" id=\"continueButton\" value=\"Try Again?\">";
+        document.getElementById("continueButton").addEventListener("click", function() {
+            location.reload();
+            document.getElementById("continueButton").remove();
+        });
     }
     const getQuestions = function(amount, difficulty, category) {
         document.getElementById("loadingContainer").style.display = "block";
@@ -98,15 +113,14 @@ const app = (function() {
             for (let i = 0; i < responseJson.results.length; i++) {
                 const result = responseJson.results[i];
                 const decodedAnswers = [];
-                result.incorrect_answers.forEach((answer) => decodedAnswers.push(decodeHtml(answer)));
+                result.incorrect_answers.forEach((answer) => decodedAnswers.push(decodeHtml(answer.trim())));
                 const question = {
-                    question: decodeHtml(result.question),
-                    correct_answer: decodeHtml(result.correct_answer),
+                    question: decodeHtml(result.question.trim()),
+                    correct_answer: decodeHtml(result.correct_answer.trim()),
                     incorrect_answers: decodedAnswers
                 };
                 const answers = question.incorrect_answers.slice();
                 answers.push(question.correct_answer);
-                debugger;
                 shuffleArray(answers);
                 question.answers = answers;
                 questions.push(question);
@@ -133,23 +147,34 @@ const app = (function() {
         document.getElementById("timer").classList.remove("running-out");
         convertToMinutesAndSeconds(questionDuration);
         timerInterval = setInterval(function () {
-            [minutes, seconds] = convertToMinutesAndSeconds(timer);
-            if (timer === Math.ceil(questionDuration / 4)) {
-                document.getElementById("timer").classList.add("running-out");
-            }
-            if (--timer < 0) {
-                timer = duration;
-                clickAnswer();
+            if (!paused) {
+                [minutes, seconds] = convertToMinutesAndSeconds(timer);
+                if (timer === Math.ceil(questionDuration / 4)) {
+                    document.getElementById("timer").classList.add("running-out");
+                }
+                if (--timer < 0) {
+                    timer = duration;
+                    clickAnswer();
+                }
+            } else {
+                timer = questionDuration;
             }
         }, 1000);
     };
     const updateQuestion = function() {
-        const question = questions[currentQuestion];
-        document.getElementById("question").innerHTML = (currentQuestion + 1) + ") " + question.question;
-        correctIndex = question.answers.indexOf(question.correct_answer);
-        const answerElements = document.getElementById("answers").querySelectorAll("li");
-        for (let i = 0; i < answerElements.length; i++) {
-            answerElements[i].innerHTML = question.answers[i];
+        if (currentQuestion === questions.length) {
+            displayResults();
+        } else {
+            paused = false;
+            const question = questions[currentQuestion];
+            document.getElementById("question").innerHTML = (currentQuestion + 1) + ") " + question.question;
+            correctIndex = question.answers.indexOf(question.correct_answer);
+            const answerElements = document.getElementById("answers").querySelectorAll("li");
+            for (let i = 0; i < answerElements.length; i++) {
+                answerElements[i].innerHTML = question.answers[i];
+                answerElements[i].classList = [];
+            }
+            startTimer(questionDuration);
         }
     };
     const init = function() {
